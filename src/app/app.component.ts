7f2d41cbad5e74654d954ec8003e226cf66259a8
Milyutkin <i.milyutkin@itransition.com>
2017-06-29 18:33:23 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'Hello World!';
+    source: string[] = ['One', 'Two', 'Three'];
+    info: string[];
+    public events: string[] = [];
+
+    constructor() {
+      this.info = this.source.slice();
+    }
+
+    onSaveClick() {
+        this.title = 'Save';
+    }
+
+    onCancelClick() {
+        this.title = 'Cancel';
+    }
+
+    public selectionChange(value: any): void {
+      console.log(value);
+    }
+
+    public filterChange(filter: any): void {
+        this.info = this.source.filter((s) => s.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
+    }
 }
